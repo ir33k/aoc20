d1affa352e97fe4458099da1730503cc64c74ce8
irek <mail@gumen.pl>
2020-12-09 22:04:41 +0100

    Solve second part of 08

--- 08.c
+++ 08.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 #define MAX_INSTRUCTIONS 650
-#define MAX_LINE_LENGTH 32
 
 typedef struct {
   char type;
@@ -11,11 +10,10 @@ typedef struct {
 } instruction;
 
 int
-parse_input(instruction instructions[MAX_INSTRUCTIONS],
-            char file_name[8]) {
+parse_input(instruction instructions[MAX_INSTRUCTIONS], char file_name[8]) {
   int i;
   FILE * file = fopen(file_name, "r");
-  char line[MAX_LINE_LENGTH];
+  char line[32];
   char number_char[8];
   int instructions_count = 0;
 
@@ -42,65 +40,65 @@ check(instruction instructions[MAX_INSTRUCTIONS], int instructions_count,
   while (i < instructions_count) {
     if (instructions[i].visited) return 0;
 
+    instructions[i].visited = 1;
     if (instructions[i].type == 'a') *acc += instructions[i].value;
     if (instructions[i].type == 'j') i    += instructions[i].value - 1;
-
-    instructions[i++].visited = 1;
+    i++;
   }
   return 1;
 }
 
 int
-solve1(instruction instructions[MAX_INSTRUCTIONS], int instructions_count) {
+solve1(char file_name[8]) {
+  instruction instructions[MAX_INSTRUCTIONS];
+  int instructions_count = parse_input(instructions, file_name);
   int acc = 0;
-  printf(">>>> %d\n", check(instructions, instructions_count, 0, &acc));
+  check(instructions, instructions_count, 0, &acc);
   return acc;
 }
 
-/* int */
-/* solve2(instruction instructions[MAX_INSTRUCTIONS]) { */
-/*   int i, j; */
-/*   int visited[MAX_INSTRUCTIONS]; */
-/*   int visited_count = 0; */
-/*   int acc = 0; */
-/*   int change_index; */
 
-/*   i = 0; */
-/*   while (1) { */
-/*     for (j=0; j<visited_count; j++) */
-/*       if (visited[j] == i) return acc; */
+int
+solve2(char file_name[8]) {
+  int i;
+  instruction instructions[MAX_INSTRUCTIONS];
+  int instructions_count = parse_input(instructions, file_name);
+  int acc;
+  int change_index = 0;
 
-/*     visited[visited_count] = i; */
-/*     visited_count++; */
+  while (1) {
+    acc = 0;
+    i = change_index;
 
-/*     switch (instructions[i].type) { */
-/*     case 'a': acc += instructions[i].value; break; */
-/*     case 'j': i   += instructions[i].value; continue; */
-/*     } */
+    while (instructions[i].type == 'a') i++;
 
-/*     i++; */
-/*   } */
+    instructions[i].type = instructions[i].type == 'j' ? 'n': 'j';
+    change_index = i;
 
-/*   /\* NOTE thie return is not rly needed *\/ */
-/*   return 0; */
-/* } */
+    if (check(instructions, instructions_count, 0, &acc)) break;
+
+    for (i=0; instructions[i].visited == 1; i++) {
+      instructions[i].visited = 0;
+      if (instructions[i].type == 'j') i += instructions[i].value - 1;
+    }
+
+    instructions[change_index].type =
+      instructions[change_index].type == 'j' ? 'n': 'j';
+
+    change_index++;
+  }
+
+  return acc;
+}
 
 int
 main() {
-  instruction instructions1[MAX_INSTRUCTIONS];
-  instruction instructions2[MAX_INSTRUCTIONS];
-  instruction instructions3[MAX_INSTRUCTIONS];
+  printf("%d (5)\n",    solve1("08i1"));
+  printf("%d (1744)\n", solve1("08i2"));
+  printf("%d (8)\n",    solve1("08i3"));
 
-  int instructions1_count = parse_input(instructions1, "08i1");
-  int instructions2_count = parse_input(instructions2, "08i2");
-  int instructions3_count = parse_input(instructions3, "08i3");
-
-  printf("%d (5)\n",    solve1(instructions1, instructions1_count));
-  printf("%d (1744)\n", solve1(instructions2, instructions2_count));
-  printf("%d (8)\n",    solve1(instructions3, instructions3_count));
-
-  /* printf("%d (8)\n",    solve2(instructions1)); */
-  /* printf("%d (1744)\n", solve1(instructions2)); */
+  printf("%d (8)\n",    solve2("08i1"));
+  printf("%d (1174)\n", solve2("08i2"));
 
   return 0;
 }
