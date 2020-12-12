07418722d939ab94a775f2d54025bf54df1e0bb1
irek <mail@gumen.pl>
2020-12-12 14:21:52 +0100

    refactor

--- 12.c
+++ 12.c
@@ -1,133 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_INSTRUCTIONS 800
-
-typedef struct {
-  char type;
-  int value;
-} instruction;
-
-/* This also can be read as X (+right, -left) and Y (+up, -down) */
-typedef struct {
-  int east_west;       /* + value is east (right), - is west (left) */
-  int north_south;     /* + value is north (up), - south (down) */
-  int rotation;        /* 0 == east (right) */
-} position;
-
 int
-parse_input(char file_name[8],
-            instruction instructions[MAX_INSTRUCTIONS]) {
-  int i;
-  int instructions_count = 0;
+solve1(char file_name[8]) {
   FILE * file = fopen(file_name, "r");
   char line[8];
-  char number_char[8];
+
+  char type;                    /* type of instruction */
+  int value;                    /* instruction value */
+
+  int pos_x = 0;                /* its east, west (+right, -left) */
+  int pos_y = 0;                /* its north, south (+up, -down) */
+  int rotation = 0;
 
   if (file == NULL) return -1;
 
   while(fgets(line, sizeof line, file) != NULL) {
-    instructions[instructions_count].type = line[0];
+    type = line[0];
+    /* Making first char "0" makes the whole like a valid int */
+    line[0] = '0';
+    value = atoi(line);
 
-    for (i=0; line[i+1] != '\n'; i++) number_char[i] = line[i+1];
-    number_char[i] = 0;
-
-    instructions[instructions_count].value = atoi(number_char);
-    instructions_count++;
-  }
-
-  return instructions_count;
-}
-
-int
-solve1(char file_name[8]) {
-  int i;
-  instruction instructions[MAX_INSTRUCTIONS];
-  int instructions_count = parse_input(file_name, instructions);
-
-  position pos = { 0, 0, 0 };
-
-  for (i=0; i<instructions_count; i++) {
-    switch (instructions[i].type) {
-    case 'E': pos.east_west   += instructions[i].value; break;
-    case 'W': pos.east_west   -= instructions[i].value; break;
-    case 'N': pos.north_south += instructions[i].value; break;
-    case 'S': pos.north_south -= instructions[i].value; break;
-    case 'L':
-      pos.rotation -= instructions[i].value;
-      if (pos.rotation < 0) pos.rotation += 360;
-      break;
-    case 'R':
-      pos.rotation += instructions[i].value;
-      if (pos.rotation >= 360) pos.rotation -= 360;
-      break;
-    case 'F':
-      switch (pos.rotation) {
-      case   0: pos.east_west   += instructions[i].value; break;
-      case 180: pos.east_west   -= instructions[i].value; break;
-      case 270: pos.north_south += instructions[i].value; break;
-      case  90: pos.north_south -= instructions[i].value; break;
+    /* For type F use one of directions base on current rotation. */
+    if (type == 'F')
+      switch (rotation) {
+      case   0: type = 'E'; break; case 180: type = 'W'; break;
+      case 270: type = 'N'; break; case  90: type = 'S'; break;
       }
+
+    /* Rotation left, moving west and south are basically the same
+       thing like corresponding types but with negative value */
+    if (type == 'L' || type == 'W' || type == 'S') value *= -1;
+
+    switch (type) {
+    case 'E': case 'W': pos_x += value; continue;
+    case 'N': case 'S': pos_y += value; continue;
+    case 'R': case 'L': rotation = (360 + rotation + value) % 360;
     }
   }
 
-  if (pos.east_west   < 0) pos.east_west   *= -1;
-  if (pos.north_south < 0) pos.north_south *= -1;
-
-  return pos.east_west + pos.north_south;
+  fclose(file);
+  return abs(pos_x) + abs(pos_y);
 }
 
 int
 solve2(char file_name[8]) {
-  int i;
-  instruction instructions[MAX_INSTRUCTIONS];
-  int instructions_count = parse_input(file_name, instructions);
+  FILE * file = fopen(file_name, "r");
+  char line[8];
 
-  int tmp_pos_value;
-  position pos_ship = { 0, 0, 0 };
-  position pos_waypoint = { 10, 1, 0 };
+  char type;                    /* type of instruction */
+  int value;                    /* instruction value */
 
-  for (i=0; i<instructions_count; i++) {
-    switch (instructions[i].type) {
-    case 'E': pos_waypoint.east_west   += instructions[i].value; break;
-    case 'W': pos_waypoint.east_west   -= instructions[i].value; break;
-    case 'N': pos_waypoint.north_south += instructions[i].value; break;
-    case 'S': pos_waypoint.north_south -= instructions[i].value; break;
-    case 'L':
-    case 'R':
-      if (instructions[i].type == 'R') {
-        /**/ if (instructions[i].value == 90) instructions[i].value = 270;
-        else if (instructions[i].value == 270) instructions[i].value = 90;
-      }
+  int tmp_pos;
+  int ship_x = 0,  ship_y = 0;
+  int wayp_x = 10, wayp_y = 1;  /* NOTE initial values 10, 1*/
 
-      switch (instructions[i].value) {
+  if (file == NULL) return -1;
+
+  while(fgets(line, sizeof line, file) != NULL) {
+    type = line[0];
+    /* Overwrite first line character with "0" to make the whole like
+       a valid number for "atoi" function. */
+    line[0] = '0';
+    value = atoi(line);
+
+    if (type == 'W' || type == 'S') value *= -1;
+
+    switch (type) {
+    case 'E': case 'W': wayp_x += value; break;
+    case 'N': case 'S': wayp_y += value; break;
+    case 'L': case 'R':
+      if (type == 'R' && (value == 90 || value == 270))
+        value = (value + 180) % 360;
+
+      switch (value) {
       case 90:
-        tmp_pos_value            = pos_waypoint.east_west;
-        pos_waypoint.east_west   = pos_waypoint.north_south * -1;
-        pos_waypoint.north_south = tmp_pos_value;
+        tmp_pos = wayp_x;
+        wayp_x = wayp_y * -1;
+        wayp_y = tmp_pos;
         break;
       case 180:
-        pos_waypoint.east_west   *= -1;
-        pos_waypoint.north_south *= -1;
+        wayp_x *= -1;
+        wayp_y *= -1;
         break;
       case 270:
-        tmp_pos_value            = pos_waypoint.east_west;
-        pos_waypoint.east_west   = pos_waypoint.north_south;
-        pos_waypoint.north_south = tmp_pos_value * -1;
+        tmp_pos = wayp_x;
+        wayp_x = wayp_y;
+        wayp_y = tmp_pos * -1;
         break;
       }
       break;
-     case 'F':
-      pos_ship.east_west   += pos_waypoint.east_west   * instructions[i].value;
-      pos_ship.north_south += pos_waypoint.north_south * instructions[i].value;
+    case 'F':
+      ship_x += wayp_x * value;
+      ship_y += wayp_y * value;
       break;
     }
   }
 
-  if (pos_ship.east_west   < 0) pos_ship.east_west   *= -1;
-  if (pos_ship.north_south < 0) pos_ship.north_south *= -1;
-
-  return pos_ship.east_west + pos_ship.north_south;
+  fclose(file);
+  return abs(ship_x) + abs(ship_y);
 }
 
 int
