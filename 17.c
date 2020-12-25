08fd16b055230b81ab3afb39cf73bafd000e36a1
irek <mail@gumen.pl>
2020-12-25 19:25:28 +0100

    Make 17 work for 4 dimensions

--- 17.c
+++ 17.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
-typedef struct { int x; int y; int z; } Point;
+#define MAX_POINTS 8192
+
+typedef struct { int x; int y; int z; int w; } Point;
 
 int
 points_get_neighbors_count(Point * points, int * points_count,
                            Point * target) {
-  int i,x,y,z, result = 0;
+  int i,x,y,z,w, result = 0;
 
   for (x=-1; x<2; x++)
     for (y=-1; y<2; y++)
       for (z=-1; z<2; z++)
-        for (i=0; i < *points_count; i++)
-          if (points[i].x == target->x + x &&
-              points[i].y == target->y + y &&
-              points[i].z == target->z + z)
-            result++;
+        for (w=-1; w<2; w++)
+          for (i=0; i < *points_count; i++)
+            if (points[i].x == target->x + x &&
+                points[i].y == target->y + y &&
+                points[i].z == target->z + z &&
+                points[i].w == target->w + w)
+              result++;
 
   return result - 1;
 }
@@ -37,8 +41,8 @@ solve1(char file_name[6], int cycles) {
   char line[16];
   int i,j,k, active_neighbors;
 
-  int x=0, y=0, z=0;
-  Point points1[1024], points2[1024], neighbor;
+  int x=0, y=0, z=0, w=0;
+  Point points1[MAX_POINTS], points2[MAX_POINTS], neighbor;
   int points1_count = 0, points2_count = 0;
 
   while (fgets(line, sizeof line, file) != NULL) {
@@ -47,6 +51,7 @@ solve1(char file_name[6], int cycles) {
         points1[points1_count].x = x;
         points1[points1_count].y = y;
         points1[points1_count].z = z;
+        points1[points1_count].w = w;
         points1_count++;
       }
 
@@ -73,39 +78,42 @@ solve1(char file_name[6], int cycles) {
 
       for (x=-1; x<2; x++)
         for (y=-1; y<2; y++)
-          for (z=-1; z<2; z++) {
-            neighbor.x = points1[j].x + x;
-            neighbor.y = points1[j].y + y;
-            neighbor.z = points1[j].z + z;
+          for (z=-1; z<2; z++)
+            for (w=-1; w<2; w++) {
+              neighbor.x = points1[j].x + x;
+              neighbor.y = points1[j].y + y;
+              neighbor.z = points1[j].z + z;
+              neighbor.w = points1[j].w + w;
 
-            for (k=0; k < points1_count; k++)
-              if (points1[k].x == neighbor.x &&
-                  points1[k].y == neighbor.y &&
-                  points1[k].z == neighbor.z)
-                goto skip;
+              for (k=0; k < points1_count; k++)
+                if (points1[k].x == neighbor.x &&
+                    points1[k].y == neighbor.y &&
+                    points1[k].z == neighbor.z &&
+                    points1[k].w == neighbor.w)
+                  goto skip;
             
-            for (k=0; k < points2_count; k++)
-              if (points2[k].x == neighbor.x &&
-                  points2[k].y == neighbor.y &&
-                  points2[k].z == neighbor.z)
-                goto skip;
+              for (k=0; k < points2_count; k++)
+                if (points2[k].x == neighbor.x &&
+                    points2[k].y == neighbor.y &&
+                    points2[k].z == neighbor.z &&
+                    points2[k].w == neighbor.w)
+                  goto skip;
 
-            active_neighbors = points_get_neighbors_count(points1, &points1_count, &neighbor);
+              active_neighbors = points_get_neighbors_count(points1, &points1_count, &neighbor);
 
-            /* printf("neighbor (%d) %d,%d,%d\n", */
-            /*        active_neighbors, */
-            /*        neighbor.x, */
-            /*        neighbor.y, */
-            /*        neighbor.z); */
+              /* printf("neighbor (%d) %d,%d,%d\n", */
+              /*        active_neighbors, */
+              /*        neighbor.x, */
+              /*        neighbor.y, */
+              /*        neighbor.z); */
 
-            if (active_neighbors + 1 == 3) {
-              points2[points2_count] = neighbor;
-              points2_count++;
+              if (active_neighbors + 1 == 3) {
+                points2[points2_count] = neighbor;
+                points2_count++;
+              }
+
+            skip: continue;
             }
-
-          skip: continue;
-          }
-
     }
 
     points_copy(points2, &points2_count, points1, &points1_count);
@@ -119,7 +127,7 @@ solve1(char file_name[6], int cycles) {
 
 int
 main() {
-  printf("%d (112)\n", solve1("17i1", 6));
-  printf("%d (395)\n", solve1("17i2", 6));
+  printf("%d (848)\n",  solve1("17i1", 6));
+  printf("%d (2296)\n", solve1("17i2", 6));
   return 0;
 }
