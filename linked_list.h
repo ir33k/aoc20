84121cb5f9937848cd75f8c7e0dad9a37d2ca8e9
irek <mail@gumen.pl>
2021-01-01 23:16:50 +0100

    Extend linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -9,6 +9,28 @@ struct linked_list_node {
   struct linked_list_node * next;
 };
 
+struct linked_list_node *
+linked_list_node_create(void * const value) {
+  struct linked_list_node * node;
+
+  node = malloc(sizeof(struct linked_list_node));
+
+  node->value = value;
+  node->prev = NULL;
+  node->next = NULL;
+
+  return node;
+}
+
+void
+linked_list_node_delete(struct linked_list_node * const node,
+                        const int free_value) {
+  if (free_value)
+    free(node->value);
+
+  free(node);
+}
+
 struct linked_list {
   struct linked_list_node * head;
   struct linked_list_node * tail;
@@ -22,33 +44,31 @@ linked_list_init(struct linked_list * const linked_list) {
   linked_list->size = 0;
 }
 
-void
+int
 linked_list_destroy(struct linked_list * const linked_list,
                     const int free_values) {
+  if (linked_list->size == 0)
+    return 0;
+
   struct linked_list_node * current = linked_list->head;
   struct linked_list_node * next = current->next;
 
-  size_t i = linked_list->size;
-
-  while (i--) {
-    if (free_values)
-      free(current->value);
-
-    free(current);
+  while (linked_list->size > 0) {
+    linked_list_node_delete(current, free_values);
+    linked_list->size -= 1;
     current = next;
 
     if (current != NULL)
       next = current->next;
   }
+
+  return 1;
 }
 
-struct linked_list_node *
-linked_list_push_after(struct linked_list * const linked_list,
-                       void * const value,
-                       struct linked_list_node * const prev) {
-  struct linked_list_node * node = malloc(sizeof(struct linked_list_node));
-
-  node->value = value;
+void
+linked_list_push_node_after(struct linked_list * const linked_list,
+                            struct linked_list_node * const node,
+                            struct linked_list_node * const prev) {
   node->prev = prev;
   node->next = prev->next;
 
@@ -57,27 +77,39 @@ linked_list_push_after(struct linked_list * const linked_list,
 
   prev->next = node;
   linked_list->size += 1;
-
-  return node;
 }
 
 void
-linked_list_push(struct linked_list * const linked_list,
-                 void * const value) {
-  if (linked_list->tail) {
-    linked_list->tail = linked_list_push_after(linked_list, value,
-                                               linked_list->tail);
-  } else {
-    struct linked_list_node * node = malloc(sizeof(struct linked_list_node));
-
-    node->value = value;
+linked_list_push_node(struct linked_list * const linked_list,
+                      struct linked_list_node * const node) {
+  if (linked_list->tail == NULL) {
     node->next = NULL;
     node->prev = NULL;
 
     linked_list->head = node;
     linked_list->tail = node;
     linked_list->size = 1;
+  } else {
+    linked_list_push_node_after(linked_list, node, linked_list->tail);
+    linked_list->tail = node;
   }
+}
+
+struct linked_list_node *
+linked_list_push_value_after(struct linked_list * const linked_list,
+                             void * const value,
+                             struct linked_list_node * const prev) {
+  struct linked_list_node * node = linked_list_node_create(value);
+  linked_list_push_node_after(linked_list, node, prev);
+  return node;
+}
+
+struct linked_list_node *
+linked_list_push_value(struct linked_list * const linked_list,
+                       void * const value) {
+  struct linked_list_node * node = linked_list_node_create(value);
+  linked_list_push_node(linked_list, node);
+  return node;
 }
 
 void *
@@ -103,41 +135,64 @@ linked_list_get_value(struct linked_list * const linked_list,
   return node ? node->value : NULL;
 }
 
+struct linked_list_node *
+linked_list_pull_by_node(struct linked_list * const linked_list,
+                         struct linked_list_node * const node) {
+  /* Make list empty if its the last node, else update list */
+  if (node->prev == NULL &&
+      node->next == NULL) {
+    linked_list->head = NULL;
+    linked_list->tail = NULL;
+  } else {
+    /* Its a head */
+    if (node->prev == NULL) {
+      linked_list->head = node->next;
+      linked_list->head->prev = NULL;
+    } else {
+      node->prev->next = node->next;
+    }
+
+    /* Its a tail */
+    if (node->next == NULL) {
+      linked_list->tail = node->prev;
+      linked_list->tail->next = NULL;
+    } else {
+      node->next->prev = node->prev;
+    }
+  }
+
+  linked_list->size -= 1;
+
+  node->next = NULL;
+  node->prev = NULL;
+
+  return node;
+}
+
+struct linked_list_node *
+linked_list_pull_by_index(struct linked_list * const linked_list,
+                          const size_t index) {
+  if (index >= linked_list->size)
+    return NULL;
+
+  struct linked_list_node * node;
+  node = linked_list_get_node(linked_list, index);
+
+  return linked_list_pull_by_node(linked_list, node);
+}
+
 void
 linked_list_delete_by_node(struct linked_list * const linked_list,
                            struct linked_list_node * const node,
                            const int free_value) {
-  /* Its a head */
-  if (node->prev == NULL) {
-    linked_list->head = node->next;
-    linked_list->head->prev = NULL;
-  } else {
-    node->prev->next = node->next;
-  }
-
-  /* Its a tail */
-  if (node->next == NULL) {
-    linked_list->tail = node->prev;
-    linked_list->tail->next = NULL;
-  } else {
-    node->next->prev = node->prev;
-  }
-
-  if (free_value)
-    free(node->value);
-
-  free(node);
-
-  linked_list->size -= 1;
+  linked_list_pull_by_node(linked_list, node);
+  linked_list_node_delete(node, free_value);
 }
 
 int
 linked_list_delete_by_index(struct linked_list * const linked_list,
                             const size_t index,
                             const int free_value) {
-  if (index >= linked_list->size)
-    return 0;
-
   struct linked_list_node * node;
   node = linked_list_get_node(linked_list, index);
 
--- linked_list_tests.c
+++ linked_list_tests.c
@@ -9,34 +9,142 @@ linked_list_log_all(struct linked_list * const linked_list,
     return 0;
 
   struct linked_list_node * node = linked_list->head;
-  size_t i = 0;
+  size_t i;
 
-  while (node) {
-    fprintf(target, "%2lu -> %d\n", i++, *(int *)node->value);
-    node = node->next;
-  }
+  for (i=0; i<linked_list->size; i++, node = node->next)
+    fprintf(target, "%2lu -> %d\n", i, *(int *)node->value);
 
   return 1;
 }
 
-int
-main() {
-  int values[5] = { 2, 4, 8, 16, 32 };
+void
+init_and_destroy() {
+  int value = 1;
   struct linked_list list;
-  struct linked_list_node * node;
+
+  /* Init and destroy empty list */
+  linked_list_init(&list);
+
+  /*
+   * NOTE Function return 0 because nothing was destroyed.  That is
+   *      expected from empty list.
+   */
+  assert(0 == linked_list_destroy(&list, 0));
+
+  assert(list.size == 0);
+
+  /* Init and destroy list with single value */
+  linked_list_init(&list);
+  linked_list_push_value(&list, &value);
+
+  assert(list.size == 1);
+
+  assert(1 == linked_list_destroy(&list, 0));
+
+  assert(list.size == 0);
+
+  /* Init and destroy list with values */
+  linked_list_init(&list);
+
+  linked_list_push_value(&list, &value);
+  linked_list_push_value(&list, &value);
+  linked_list_push_value(&list, &value);
+  linked_list_push_value(&list, &value);
+  linked_list_push_value(&list, &value);
+
+  assert(list.size == 5);
+
+  assert(1 == linked_list_destroy(&list, 0));
+
+  assert(list.size == 0);
+}
+
+void
+head_and_tail() {
+  int value = 1;
+  struct linked_list list;
+
+  /* Just initialized empty list don't have head and tail */
+  linked_list_init(&list);
+
+  assert(list.size == 0);
+  assert(list.head == NULL);
+  assert(list.tail == NULL);
+
+  /* If list have single node its head and tail at the same time */
+  linked_list_push_value(&list, &value);
+
+  assert(list.size == 1);
+  assert(list.head == list.tail);
+  assert(list.head->next == NULL);
+  assert(list.head->prev == NULL);
+
+  /* If list have 2 nodes its just head and tail */
+  linked_list_push_value(&list, &value);
+
+  assert(list.head != list.tail);
+  assert(list.head->next == list.tail);
+  assert(list.tail->prev == list.head);
+  assert(list.head->prev == NULL);
+  assert(list.tail->next == NULL);
+
+  /* Deleting head or tail nodes should update head and tail list
+     pointers */
+  linked_list_delete_by_index(&list, 0, 0);
+
+  assert(list.size == 1);
+  assert(list.head == list.tail);
+  assert(list.head->next == NULL);
+  assert(list.head->prev == NULL);
+
+  /* Making list empty again should result in no head and tail */
+  linked_list_delete_by_index(&list, 0, 0);
+
+  assert(list.size == 0);
+  assert(list.head == NULL);
+  assert(list.tail == NULL);
+
+  /* Test removing again with multiple values */
+  linked_list_push_value(&list, &value);
+  linked_list_push_value(&list, &value);
+  linked_list_push_value(&list, &value);
+
+  assert(list.size == 3);
+  assert(list.head != list.tail);
+
+  linked_list_delete_by_index(&list, 0, 0);
+  linked_list_delete_by_index(&list, 0, 0);
+
+  assert(list.size == 1);
+  assert(list.head == list.tail);
+  assert(list.head->next == NULL);
+  assert(list.head->prev == NULL);
+
+  linked_list_delete_by_index(&list, 0, 0);
+
+  assert(list.size == 0);
+  assert(list.head == NULL);
+  assert(list.tail == NULL);
+
+  linked_list_destroy(&list, 0);
+}
+
+void
+push_value() {
+  int values[6] = { 2, 4, 8, 16, 32, 64 };
+  struct linked_list list;
+  struct linked_list_node * prev;
 
   linked_list_init(&list);
 
   assert(list.size == 0);
 
-  linked_list_push(&list, &values[0]);
-
-  assert(list.head == list.tail);
-  
-  linked_list_push(&list, &values[1]);
-  linked_list_push(&list, &values[2]);
-  linked_list_push(&list, &values[3]);
-  linked_list_push(&list, &values[4]);
+  /* Push 5 values */
+  linked_list_push_value(&list, &values[0]);
+  linked_list_push_value(&list, &values[1]);
+  linked_list_push_value(&list, &values[2]);
+  linked_list_push_value(&list, &values[3]);
+  linked_list_push_value(&list, &values[4]);
 
   assert(list.size == 5);
   assert(values[0] == *(int *)linked_list_get_value(&list, 0));
@@ -45,49 +153,87 @@ main() {
   assert(values[3] == *(int *)linked_list_get_value(&list, 3));
   assert(values[4] == *(int *)linked_list_get_value(&list, 4));
 
-  assert(linked_list_delete_by_index(&list, 1, 0));
-  assert(linked_list_delete_by_index(&list, 1, 0));
+  /* Push more values after some node */
+  prev = linked_list_get_node(&list, 2);
 
-  assert(list.size == 3);
-  assert(values[0] == *(int *)linked_list_get_value(&list, 0));
-  assert(values[3] == *(int *)linked_list_get_value(&list, 1));
-  assert(values[4] == *(int *)linked_list_get_value(&list, 2));
+  linked_list_push_value_after(&list, &values[5], prev);
+  linked_list_push_value_after(&list, &values[5], prev);
+  linked_list_push_value_after(&list, &values[5], prev);
 
-  node = linked_list_get_node(&list, 0);
-
-  linked_list_push_after(&list, &values[2], node);
-  linked_list_push_after(&list, &values[1], node);
-
-  assert(list.size == 5);
+  assert(list.size == 8);
   assert(values[0] == *(int *)linked_list_get_value(&list, 0));
   assert(values[1] == *(int *)linked_list_get_value(&list, 1));
   assert(values[2] == *(int *)linked_list_get_value(&list, 2));
-  assert(values[3] == *(int *)linked_list_get_value(&list, 3));
-  assert(values[4] == *(int *)linked_list_get_value(&list, 4));
-
-  linked_list_delete_by_node(&list, list.tail, 0);
-  linked_list_delete_by_node(&list, list.head, 0);
-
-  assert(list.head == linked_list_get_node(&list, 0));
-  assert(list.tail == linked_list_get_node(&list, list.size-1));
-
-  assert(list.head->prev == NULL);
-  assert(list.tail->next == NULL);
-
-  assert(list.size == 3);
-  assert(values[1] == *(int *)linked_list_get_value(&list, 0));
-  assert(values[2] == *(int *)linked_list_get_value(&list, 1));
-  assert(values[3] == *(int *)linked_list_get_value(&list, 2));
-
-  assert(linked_list_delete_by_index(&list, 1, 0) == 1);
-  assert(linked_list_delete_by_index(&list, 1, 0) == 1);
-
-  assert(list.size == 1);
-  assert(list.head == list.tail);
-
-  linked_list_log_all(&list, stdout);
+  assert(values[5] == *(int *)linked_list_get_value(&list, 3));
+  assert(values[5] == *(int *)linked_list_get_value(&list, 4));
+  assert(values[5] == *(int *)linked_list_get_value(&list, 5));
+  assert(values[3] == *(int *)linked_list_get_value(&list, 6));
+  assert(values[4] == *(int *)linked_list_get_value(&list, 7));
 
   linked_list_destroy(&list, 0);
+}
+
+void
+push_node() {
+  int values[8] = { 2, 4, 8, 16, 32, 64, 128, 256 };
+  struct linked_list list;
+  struct linked_list_node * prev;
+  struct linked_list_node * nodes[8];
+
+  for (int i = 0; i < 8; i++)
+    nodes[i] = linked_list_node_create(&values[i]);
+
+  linked_list_init(&list);
+
+  assert(list.size == 0);
+
+  /* Push 5 values */
+  linked_list_push_node(&list, nodes[0]);
+  linked_list_push_node(&list, nodes[1]);
+  linked_list_push_node(&list, nodes[2]);
+  linked_list_push_node(&list, nodes[3]);
+  linked_list_push_node(&list, nodes[4]);
+
+  assert(list.size == 5);
+  assert(*(int *)nodes[0]->value == *(int *)linked_list_get_value(&list, 0));
+  assert(*(int *)nodes[1]->value == *(int *)linked_list_get_value(&list, 1));
+  assert(*(int *)nodes[2]->value == *(int *)linked_list_get_value(&list, 2));
+  assert(*(int *)nodes[3]->value == *(int *)linked_list_get_value(&list, 3));
+  assert(*(int *)nodes[4]->value == *(int *)linked_list_get_value(&list, 4));
+
+  /* Push more values after some node */
+  prev = linked_list_get_node(&list, 2);
+
+  /*
+   * WARNING Remember that each node have to be new.  If you put the
+   *         same node in two different places in list it will cause
+   *         problems. It's usually easier to add new values using
+   *         push_value functions.
+   */
+  linked_list_push_node_after(&list, nodes[7], prev);
+  linked_list_push_node_after(&list, nodes[6], prev);
+  linked_list_push_node_after(&list, nodes[5], prev);
+
+  assert(list.size == 8);
+  assert(*(int *)nodes[0]->value == *(int *)linked_list_get_value(&list, 0));
+  assert(*(int *)nodes[1]->value == *(int *)linked_list_get_value(&list, 1));
+  assert(*(int *)nodes[2]->value == *(int *)linked_list_get_value(&list, 2));
+  assert(*(int *)nodes[5]->value == *(int *)linked_list_get_value(&list, 3));
+  assert(*(int *)nodes[6]->value == *(int *)linked_list_get_value(&list, 4));
+  assert(*(int *)nodes[7]->value == *(int *)linked_list_get_value(&list, 5));
+  assert(*(int *)nodes[3]->value == *(int *)linked_list_get_value(&list, 6));
+  assert(*(int *)nodes[4]->value == *(int *)linked_list_get_value(&list, 7));
+
+  linked_list_destroy(&list, 0);
+}
+
+int
+main() {
+  init_and_destroy();
+  head_and_tail();
+  push_value();
+  push_node();
+  /* TODO add more tests */
 
   return 0;
 }
